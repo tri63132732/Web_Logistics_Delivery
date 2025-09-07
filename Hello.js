b3e4ba277c09e1522d83791014849734a55917a1
tri63132732 <144207384+tri63132732@users.noreply.github.com>
2025-09-07 21:38:08 +0700

    Update

---
+++ Hello.js
@@ -0,0 +1,9 @@
+import express from "express";
+
+const app = express();
+
+app.get("/hello", (req, res) => {
+  res.json("Hello World" );
+});
+
+app.listen(3000, () => console.log("Server running on http://localhost:3000"));
